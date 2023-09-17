4dd67884da251d1c099f15db1c0a3f643e75a57d
Javi Laveda <javi.laveda.dev@gmail.com>
2023-09-17 11:57:58 +0200

    first upload

--- src/main.ts
+++ src/main.ts
@@ -1,107 +1,176 @@
 import "./style.css";
 
-// function increase() {
-//   const turnElement = (document.getElementById("turn") as HTMLInputElement).innerText;
-//   if (turnElement !== null && turnElement !== undefined) {
-//     const turn = parseInt(turnElement) + 1;
-//     turnElement.innerText = turn.toString().padStart(2, "0");
+type Especialidad = "Medico de familia" | "Pediatra" | "Cardiólogo" | "Otros";
+
+interface Pacientes {
+  id: number;
+  nombre: string;
+  apellidos: string;
+  sexo: string;
+  temperatura: number;
+  frecuenciaCardiaca: number;
+  especialidad: Especialidad;
+  edad: number;
+}
+
+const pacientes: Pacientes[] = [
+  {
+    id: 1,
+    nombre: "John",
+    apellidos: "Doe",
+    sexo: "Male",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 80,
+    especialidad: "Medico de familia",
+    edad: 44,
+  },
+  {
+    id: 2,
+    nombre: "Jane",
+    apellidos: "Doe",
+    sexo: "Female",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 70,
+    especialidad: "Medico de familia",
+    edad: 43,
+  },
+  {
+    id: 3,
+    nombre: "Junior",
+    apellidos: "Doe",
+    sexo: "Male",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 90,
+    especialidad: "Pediatra",
+    edad: 8,
+  },
+  {
+    id: 4,
+    nombre: "Mary",
+    apellidos: "Wien",
+    sexo: "Female",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 120,
+    especialidad: "Medico de familia",
+    edad: 20,
+  },
+  {
+    id: 5,
+    nombre: "Scarlett",
+    apellidos: "Somez",
+    sexo: "Female",
+    temperatura: 36.8,
+    frecuenciaCardiaca: 110,
+    especialidad: "Cardiólogo",
+    edad: 30,
+  },
+  {
+    id: 6,
+    nombre: "Brian",
+    apellidos: "Kid",
+    sexo: "Male",
+    temperatura: 39.8,
+    frecuenciaCardiaca: 80,
+    especialidad: "Pediatra",
+    edad: 11,
+  },
+];
+
+const muestraPaciente = (paciente: Pacientes) => {
+  const div = document.getElementById("elenco-pacientes");
+  const id = document.createElement("p");
+  const nombre = document.createElement("p");
+  const apellidos = document.createElement("p");
+  const temperatura = document.createElement("p");
+  const frecuenciaCardiaca = document.createElement("p");
+  const especialidad = document.createElement("p");
+  const edad = document.createElement("p");
+  const cardDiv = document.createElement("div");
+
+  id.textContent = "Número paciente:" + "     " + paciente.id.toString();
+  nombre.textContent = "Nombre:" + "      " + paciente.nombre;
+  apellidos.textContent = "Apellidos:" + "      " + paciente.apellidos;
+  temperatura.textContent =
+    "Temperatura:" + "      " + paciente.temperatura.toString();
+  frecuenciaCardiaca.textContent =
+    "Frecuencia cardíaca:" + "      " + paciente.frecuenciaCardiaca.toString();
+  especialidad.textContent = "Especialidad:" + "      " + paciente.especialidad;
+  edad.textContent = "Edad:" + "      " + paciente.edad.toString();
+  cardDiv.setAttribute("class", "card");
+
+  if (div && div instanceof HTMLDivElement) {
+    div.appendChild(cardDiv);
+    cardDiv.appendChild(id);
+    cardDiv.appendChild(nombre);
+    cardDiv.appendChild(apellidos);
+    cardDiv.appendChild(temperatura);
+    cardDiv.appendChild(frecuenciaCardiaca);
+    cardDiv.appendChild(especialidad);
+    cardDiv.appendChild(edad);
+  }
+};
+
+const obtenPacientesAsignadosAPediatria = (
+  listadoPacientes: Pacientes[]
+): Pacientes[] => {
+  for (let i = 0; i < listadoPacientes.length; i++) {
+    if (listadoPacientes[i].especialidad === "Pediatra") {
+      muestraPaciente(listadoPacientes[i]);
+      console.log(listadoPacientes[i]);
+    }
+  }
+  return listadoPacientes;
+};
+
+obtenPacientesAsignadosAPediatria(pacientes);
+
+// const pacientesPediatria: Pacientes[] =
+//   obtenPacientesAsignadosAPediatria(pacientes);
+
+// // muestraPaciente(pacientesPediatria);
+
+// muestraPaciente(listaPacientes)
+
+// const pacientesPediatria: Pacientes[] =
+//   obtenPacientesAsignadosAPediatria(pacientes);
+
+// const obtenPacientesAsignadosAPediatriaMenos10años = (
+//   pacientes: Pacientes[]
+// ): Pacientes[] => {
+//   for (let i = 0; i < pacientes.length; i++) {
+//     if (pacientes[i].especialidad === "Pediatra" && pacientes[i].edad < 10) {
+//       muestraPaciente(pacientes[i]);
+//     }
 //   }
-// }
+//   return pacientes;
+// };
 
-// NEXT BUTTON
-function next() {
-  //get value
-  const turnValue = (document.getElementById("turn") as HTMLInputElement)
-    .innerText;
-  //operate
-  const turnNext = parseInt(turnValue) + 1;
-  //show
-  const turnElement = document.getElementById("turn");
-  if (turnElement !== null && turnElement !== undefined) {
-    turnElement.innerHTML = turnNext.toString().padStart(2, "0");
-  }
-}
-const btnIncrease = document.getElementById("next");
+// obtenPacientesAsignadosAPediatria(pacientes);
+// obtenPacientesAsignadosAPediatriaMenos10años(pacientes);
 
-if (
-  btnIncrease !== null &&
-  btnIncrease !== undefined &&
-  btnIncrease instanceof HTMLButtonElement
-) {
-  btnIncrease.addEventListener("click", next);
-}
+// const activarProtocoloUrgencia = (pacientes: Pacientes[]): boolean => {
+//   let activarProtocolo = false;
+//   for (let i = 0; i < pacientes.length; i++) {
+//     if (
+//       pacientes[i].frecuenciaCardiaca > 100 &&
+//       pacientes[i].temperatura > 38
+//     ) {
+//       activarProtocolo = true;
+//     }
+//   }
+//   return activarProtocolo;
+// };
 
-// BACK BUTTON
+// activarProtocoloUrgencia(pacientes);
+// console.log(activarProtocoloUrgencia(pacientes));
 
-function back() {
-  //get value
-  const turnValue = (document.getElementById("turn") as HTMLInputElement)
-    .innerText;
-  //operate
-  const turnBack = parseInt(turnValue) - 1;
-  //show
-  const turnElement = document.getElementById("turn");
-  if (turnBack >= 0 && turnElement !== null && turnElement !== undefined) {
-    turnElement.innerHTML = turnBack.toString().padStart(2, "0");
-  }
-}
+// const reasignaPacientesAMedicoFamilia = (
+//   pacientes: Pacientes[]
+// ): Pacientes[] => {
+//   for (let i = 0; i < pacientes.length; i++) {
+//     pacientes[i].especialidad = "Medico de familia";
+//   }
+//   return pacientes;
+// };
 
-const btnBack = document.getElementById("back");
-
-if (
-  btnBack !== null &&
-  btnBack !== undefined &&
-  btnBack instanceof HTMLButtonElement
-) {
-  btnBack.addEventListener("click", back);
-}
-
-// RESET BUTTON
-
-function reset() {
-  //get value
-  const turnValue = (document.getElementById("turn") as HTMLInputElement)
-    .innerText;
-  //operate
-  const turnReset = parseInt(turnValue) * 0;
-  //show
-  const turnElement = document.getElementById("turn");
-  if (turnElement !== null && turnElement !== undefined) {
-    turnElement.innerHTML = turnReset.toString().padStart(2, "0");
-  }
-}
-
-const btnReset = document.getElementById("reset");
-
-if (
-  btnReset !== null &&
-  btnReset !== undefined &&
-  btnReset instanceof HTMLButtonElement
-) {
-  btnReset.addEventListener("click", reset);
-}
-
-// INPUT BUTTON
-
-function input() {
-  //get value
-  const inputValue = (document.getElementById("input") as HTMLInputElement)
-    .value;
-  //operate
-  const turnInput = parseInt(inputValue);
-  //show
-  const inputElement = document.getElementById("turn");
-  if (turnInput >= 0 && inputElement !== null && inputElement !== undefined) {
-    inputElement.innerHTML = turnInput.toString().padStart(2, "0");
-  }
-}
-
-const btnInput = document.getElementById("okInput");
-
-if (
-  btnInput !== null &&
-  btnInput !== undefined &&
-  btnInput instanceof HTMLButtonElement
-) {
-  btnInput.addEventListener("click", input);
-}
+// console.log(reasignaPacientesAMedicoFamilia(pacientesPediatria));
